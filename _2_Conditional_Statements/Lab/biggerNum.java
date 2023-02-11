2e3d1bc69f7fa977b1d65185f28b82cdb82dce38
BorisKolev94 <125131851+BorisKolev94@users.noreply.github.com>
2023-02-11 16:57:25 +0200

    biggerNumAndEvenOrOdd

---
+++ _2_Conditional_Statements/Lab/biggerNum.java
@@ -0,0 +1,19 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class biggerNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+
+        if (a > b) {
+            System.out.println(a);
+        } else if (a < b) {
+            System.out.println(b);
+        } else {
+            System.out.println(a);
+        }
+    }
+}
