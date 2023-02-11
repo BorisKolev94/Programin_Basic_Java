8f6e54e69e6d9a01ac55c38158594293150be65f
BorisKolev94 <125131851+BorisKolev94@users.noreply.github.com>
2023-02-11 16:59:21 +0200

    biggerNumAndEvenOrOdd

--- _2_Conditional_Statements/Lab/evenOrOdd.java
+++ _2_Conditional_Statements/Lab/evenOrOdd.java
@@ -6,7 +6,7 @@ public class evenOrOdd {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num1 = Integer.parseInt(scanner.nextLine());
-        if (num1%2==0){
+        if (num1 % 2 == 0) {
             System.out.println("even");
         } else {
             System.out.println("odd");
