209185bad976c866f33ea78d84901842fcc5ed37
BorisKolev94 <125131851+BorisKolev94@users.noreply.github.com>
2023-02-22 21:10:42 +0200

    numberInRange

---
+++ _3_Conditional_Statements_Advanced/Lab/numberInRange.java
@@ -0,0 +1,18 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class numberInRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double number = Double.parseDouble(scanner.nextLine());
+
+        if (number>=-100 && number<=100 && number !=0){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+
+
+        }
+    }
+}
