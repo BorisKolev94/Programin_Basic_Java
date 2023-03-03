f4993c8fb4e20ec039ee5c7cc26f11c156b58748
BorisKolev94 <125131851+BorisKolev94@users.noreply.github.com>
2023-03-03 11:56:30 +0200

    numbersEndingInSeven

---
+++ _3_Conditional_Statements_Advanced/forLoopExercise/nNumbersEndingIn7.java
@@ -0,0 +1,13 @@
+package forLoopExercise;
+
+public interface nNumbersEndingIn7 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i<1000; i++){
+            if (i%10==7){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
