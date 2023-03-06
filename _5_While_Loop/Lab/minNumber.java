5da9bd2774560b49816200fb5db2c9b0c0eea122
BorisKolev94 <125131851+BorisKolev94@users.noreply.github.com>
2023-03-06 22:40:24 +0200

    minNumber

---
+++ _5_While_Loop/Lab/minNumber.java
@@ -0,0 +1,19 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class minNumber {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        String input = scanner.nextLine();
+        int minNumber= Integer.MAX_VALUE;
+        while(!input.equals("Stop")){
+            int currentNumber= Integer.parseInt(input);
+            if (minNumber>currentNumber){
+                minNumber=currentNumber;
+            }
+            input= scanner.nextLine();
+        }
+        System.out.printf("%d", minNumber);
+    }
+}
