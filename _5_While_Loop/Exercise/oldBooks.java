87ae9388f39c1125c99299318a93626aff3561a3
BorisKolev94 <125131851+BorisKolev94@users.noreply.github.com>
2023-03-08 20:15:20 +0200

    oldBook

---
+++ _5_While_Loop/Exercise/oldBooks.java
@@ -0,0 +1,30 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class oldBooks {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String bookName = scanner.nextLine();
+        int count = 0;
+        boolean theBookIsFound = false;
+
+        String input= scanner.nextLine();
+        while (!input.equals("No More Books")) {
+            if (input.equals(bookName)) {
+                theBookIsFound = true;
+                break;
+            }
+            count++;
+            input= scanner.nextLine();
+        }
+        if (theBookIsFound){
+            System.out.printf("You checked %d books and found it.", count);
+        }else {
+            System.out.println("The book you search is not here!");
+            System.out.printf("You checked %d books.", count);
+        }
+
+    }
+}
