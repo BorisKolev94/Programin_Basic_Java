a0d90b374807d0471f328e384aeae8b996018029
BorisKolev94 <125131851+BorisKolev94@users.noreply.github.com>
2023-03-12 13:26:31 +0200

    travelling

---
+++ _06_NestedLoop/Lab/travelling.java
@@ -0,0 +1,24 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class travelling {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String destination = scanner.nextLine();
+
+        while (!destination.equals("End")) {
+            double minBudget = Double.parseDouble(scanner.nextLine());
+            double myMoney = 0;
+            while (myMoney < minBudget) {
+                double savedMoney = Double.parseDouble(scanner.nextLine());
+                myMoney += savedMoney;
+            }
+            System.out.printf("Going to %s!%n", destination);
+
+            destination = scanner.nextLine();
+        }
+
+    }
+}
